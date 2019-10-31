73b5e86356da2458feb66ebd62402b6334fc5891
Phil Dufault <phil@dufault.info>
2019-10-31 09:21:23 -0700

    committing a lot of local fixes :scream:

--- pydrivehealth.py
+++ pydrivehealth.py
@@ -2,7 +2,10 @@
 from __future__ import print_function
 import pySMART
 import tabulate
-
+import os
+import sys
+import pwd
+import argparse
 
 def get_smart_attribute(drive, attribute):
   for driveattribute in drive.attributes:
@@ -14,13 +17,13 @@ def get_smart_attribute(drive, attribute):
 
 def get_drive_age(drive):
   hours = get_smart_attribute(drive, 'Power_On_Hours')
-  return str("%8.2f" % (hours / 24 / 365)) + ' years'
+  return str('%8.2f' % (hours / 24 / 365)) + ' years'
 
 
 def get_drive_temperature(drive):
   temp = get_smart_attribute(drive, 'Temperature_Celsius')
   if temp:
-    temp = str(temp) + "C"
+    temp = str(temp) + 'C'
   return temp
 
 
@@ -28,27 +31,56 @@ def get_drive_reallocated_sectors(drive):
   return get_smart_attribute(drive, 'Reallocated_Sector_Ct')
 
 
-def display_drive_information(devlist):
-  header = [ 'Name', 'Health', 'Model', 'Serial', 'Capacity', 'Temperature', 'Age (on)', 'Reallocated Sectors']
-  data = []
+def display_drive_information(devlist, verbose):
+  header = ['Name', 'Health', 'Model', 'Capacity', 'Temperature', 'Age (on)', 'Reallocated Sectors']
+  if verbose:
+    header = ['Name', 'Health', 'Model', 'Serial', 'Capacity', 'Temperature', 'Age (on)', 'Reallocated Sectors']
+  data = [ header ]
   for device in devlist.devices:
-    data.append(get_drive_information(device))
+    data.append(get_drive_information(device, verbose))
 
-  print(tabulate.tabulate(data, header, 'rst'))
+  print('{0} drives online'.format(len(devlist.devices)))
+  print(tabulate.tabulate(data, 'firstrow' , 'rst'))
 
 
-def get_drive_information(drive):
+def get_drive_information(drive, verbose):
+  '''
+  Arguments are:
+   * drive, a pySMART devicelist object, and
+   * verbose, a boolean flag that increases the number of disk attributes this function returns
+  Returns an array of 
+  '''
+
   temp = get_drive_temperature(drive)
   age = get_drive_age(drive)
   reallocated_sectors = get_drive_reallocated_sectors(drive)
-  data = [ drive.name, drive.assessment, drive.model, drive.serial, drive.capacity, temp, age, reallocated_sectors ]
+
+  if verbose:
+    data = ( drive.name, drive.assessment, drive.model, drive.serial, drive.capacity, temp, age, reallocated_sectors )
+#    data = [ drive.name, 'PASS', 'XXXXXXXXX', 'XXXXXXXXXXXXXX', drive.capacity, temp, age, reallocated_sectors ]
+  else:
+    data = ( drive.name, drive.assessment, drive.model, drive.capacity, temp, age, reallocated_sectors )
+#    data = [ drive.name, 'PASS', '4.00 TB' , temp, age, reallocated_sectors ]
+
   return data
 
 
 def main():
-  devlist = pySMART.DeviceList()
-  display_drive_information(devlist)
+  '''
+  Ensure this script is run as root, so that it can query disk information.
+  Parse arguments and then display information on the disk drives in the system
+  '''
 
-if __name__ == "__main__":
+  if os.getuid() != 0:
+    raise RuntimeError('Run this script as root, as it needs to query disks.\nsudo {0}'.format(sys.argv[0]))
+
+  parser = argparse.ArgumentParser(description='A script to display disk attributes')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Show verbose disk attributes view') 
+  args = parser.parse_args()
+
+  devlist = pySMART.DeviceList()
+  display_drive_information(devlist, args.verbose)
+
+if __name__ == '__main__':
   main()
 
